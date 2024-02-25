afe2bb202cf5a5af05ff2bb9c2ca1731f0249644
KudriaevOS <KudriaevOS@ugsk.ru>
2024-02-25 17:02:28 +0300

    init

---
+++ src/main/java/com/programmingpatterns/ProgrammingPatternsApplication.java
@@ -0,0 +1,13 @@
+package com.programmingpatterns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgrammingPatternsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProgrammingPatternsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/programmingpatterns/ProgrammingPatternsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.programmingpatterns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgrammingPatternsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
